a5cca901e3c0987d873afc18d63a73ed70bd5f36
Angel Lopez <srterradev@gmail.com>
2026-02-24 21:35:01 -0600

    chore: playwright config for better and fast workflow execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,16 +18,14 @@ export default defineConfig({
     testIdAttribute: 'data-pw-id',
   },
   projects: isCI
-    ? [
-      { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    ]
+    ? [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }]
     : [
-      { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-      { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-      { name: 'webkit', use: { ...devices['Desktop Safari'] } },
-      { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
-      { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
-    ],
+        { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+        { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+        { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+        { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
+        { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
+      ],
   webServer: {
     command: isCI ? 'bun run start' : 'bun run dev',
     url: 'http://localhost:3000',
