0fdc99cf63c409d5b33dfd644d09026d9b34768b
Angel Lopez <srterradev@gmail.com>
2026-06-29 01:01:26 -0600

    chore: add StatsList component for displaying statistical data in a structured format

---
+++ src/components/Tasks/StatsDialog/StatsList.tsx
@@ -0,0 +1,28 @@
+import { DataList } from '@chakra-ui/react';
+import { InfoTip } from '@/components/ui/toggle-tip';
+import React, { JSX } from 'react';
+
+interface StatItem {
+  label: string;
+  value: number | string;
+  info?: string;
+  icon?: JSX.Element;
+  valueIcon?: JSX.Element;
+}
+
+export const StatsList = ({ items }: { items: StatItem[] }) => (
+  <DataList.Root orientation='horizontal'>
+    {items.map((item) => (
+      <DataList.Item key={item.label}>
+        <DataList.ItemLabel opacity={0.7} gap={2}>
+          {item.icon} {item.label}
+        </DataList.ItemLabel>
+        <DataList.ItemValue display='flex' gap={3} alignItems='center'>
+          {item.valueIcon && item.valueIcon}
+          {item.value}
+          {item.info && <InfoTip>{item.info}</InfoTip>}
+        </DataList.ItemValue>
+      </DataList.Item>
+    ))}
+  </DataList.Root>
+);
